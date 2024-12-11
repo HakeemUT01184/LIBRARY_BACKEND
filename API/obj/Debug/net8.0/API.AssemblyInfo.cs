4996e1533c955c5af2aa4a7addc42c7dad5fc0b5
hakeem <hakeemmass1999@gmail.com>
2024-12-11 19:03:42 +0530

    Backend_API

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7db627ea360ec2177e17fd36719b6dc21395212a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+714b2ba2398d73c8aa02aca814c333a2d16bc1f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
